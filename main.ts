052774f5945ff5c15bc06d46ca73cc5cbecccf2a
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-11-01 14:27:14 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,48 @@
+input.onButtonPressed(Button.A, function () {
+    clrShuttle(sx, sy)
+    sy = sy - 1
+    if (0 > sy) {
+        sy = 0
+    }
+    shoShuttle(sx, sy)
+})
+function doShift () {
+    clrShuttle(sx, sy)
+    for (let xx = 0; xx <= 3; xx++) {
+        for (let yy = 0; yy <= 4; yy++) {
+            led.plotBrightness(xx, yy, led.pointBrightness(xx + 1, yy))
+        }
+    }
+    shoShuttle(sx, sy)
+}
 function shoShuttle (x: number, y: number) {
     led.plotBrightness(x, y, 200)
+}
+input.onButtonPressed(Button.B, function () {
+    clrShuttle(sx, sy)
+    sy = sy + 1
+    if (4 < sy) {
+        sy = 4
+    }
+    shoShuttle(sx, sy)
+})
+function mkStars () {
+    for (let index = 0; index <= 4; index++) {
+        led.unplot(4, index)
+    }
+    led.plotBrightness(4, randint(1, 3), 66)
 }
 function clrShuttle (x: number, y: number) {
     led.plotBrightness(x, y, 0)
 }
+let sy = 0
 let sx = 0
-let sy = 4
+sx = 1
+sy = 4
 shoShuttle(sx, sy)
 let speed = 0
 basic.forever(function () {
-	
+    doShift()
+    mkStars()
+    basic.pause(100 + 250 * sy)
 })
